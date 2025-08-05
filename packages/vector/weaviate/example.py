93dc38a55ad8273883ca14340aa831440ace5193
Daulet Amirkhanov <damirkhanov01@gmail.com>
2025-08-05 14:54:54 +0100

    refactor: weavite/example.py use env credentials

--- packages/vector/weaviate/example.py
+++ packages/vector/weaviate/example.py
@@ -1,10 +1,10 @@
+import os
 import sys
 import asyncio
 import pathlib
-from os import path
 
 ######### Needed only for this example, not to be used in production code.
-packages_module_path = path.join(pathlib.Path(__file__).parent.parent.parent.parent)
+packages_module_path = os.path.join(pathlib.Path(__file__).parent.parent.parent.parent)
 sys.path.append(packages_module_path)
 #########
 
@@ -15,16 +15,16 @@ async def main():
     from cognee import config, prune, add, cognify, search, SearchType
 
     system_path = pathlib.Path(__file__).parent
-    config.system_root_directory(path.join(system_path, ".cognee-system"))
-    config.data_root_directory(path.join(system_path, ".cognee-data"))
+    config.system_root_directory(os.path.join(system_path, ".cognee-system"))
+    config.data_root_directory(os.path.join(system_path, ".cognee-data"))
 
     config.set_relational_db_config({
         "db_provider": "sqlite",
     })
     config.set_vector_db_config({
         "vector_db_provider": "weaviate",
-        "vector_db_url": "https://your-weaviate-instance.weaviate.network",
-        "vector_db_key": "your-api-key",
+        "vector_db_url": os.getenv("VECTOR_DB_URL"),
+        "vector_db_key": os.getenv("VECTOR_DB_KEY"),
     })
     config.set_graph_db_config({
         "graph_database_provider": "networkx",
